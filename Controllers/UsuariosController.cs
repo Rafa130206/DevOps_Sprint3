057aebe36ad0f723ef8a37a08f0320f79688dad3
dudabrigidio <dudabrigidio@gmail.com>
2025-10-01 17:45:54 -0300

    login att

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -145,9 +145,23 @@ namespace AutoTTU.Controllers
             return NoContent();
         }
 
+        [HttpPost("Login")]
+        public async Task<ActionResult<Usuario>> PostUsuario(Login login)
+        {
+            var user = _context.Usuario
+                .FirstOrDefault(u => u.Email == login.Email && u.Senha == login.Senha);
+            if (user == null)
+                return Unauthorized("Email ou senha inválidos");
+
+            var idToken = Guid.NewGuid().ToString();
+            return Ok(new { message = "Login bem-sucedido", idToken = idToken });
+        }
+
+
         private bool UsuarioExists(int id)
         {
             return _context.Usuario.Any(e => e.IdUsuario == id);
         }
     }
+
 }
---
+++ Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoTTU.Models
+{
+    public class Login
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Senha { get; set; }
+    }
+}
